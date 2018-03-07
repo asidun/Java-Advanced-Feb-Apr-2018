32c9ace52887a81a0d9360e61f26f9d2ff2b3208
asidun <asidun@gmail.com>
2018-03-07 11:38:25 +0200

    Work with DB

--- FlowerGarden/src/main/java/com/flowergarden/run/Run.java
+++ FlowerGarden/src/main/java/com/flowergarden/run/Run.java
@@ -1,10 +1,29 @@
 package com.flowergarden.run;
 
+import java.io.File;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Run {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
-
+		File file = new File("flowergarden.db");
+		String url = "jdbc:sqlite:"+file.getCanonicalFile().toURI();
+		System.out.println(url);
+		try(Connection conn = DriverManager.getConnection(url)) {			
+			Statement st = conn.createStatement();
+			ResultSet rs = st.executeQuery("select * from flower");
+			while (rs.next()) {
+				System.out.println(rs.getString(2));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} 
 
 	}
 
